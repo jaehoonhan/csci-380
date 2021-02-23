310084e19d857182c095564e106fabed6508c8ba
Jaehoon Han <JaehoonHan1@gmail.com>
2021-02-23 02:27:55 -0500

    Add matlib to counter app

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,8 +1,13 @@
 // set inital value to zero
 let count = 2;
+
+const companies = ["Uber", "Lyft", "Google", "Apple", "SpaceX", "Tinder"]
+const animals = ["Aardvark", "Blue Footed Booby", "Flying Dragon", "Giraffe Weevil", "Hammerhead Slug", "Komodo Dragon", "Naked Mole Rat", "Okapi", "Red Panda"]
+const vowels = ["A", "E", "I", "O", "U"]
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+
 
 console.log(btns)
 
@@ -11,14 +16,26 @@ console.log(btns)
 // }
 
 
+
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
-      count--;
+      const companyName = companies[Math.floor(Math.random() * companies.length)]
+
+      const firstLetter = companyName.charAt(0)
+      const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!<br />"
+    
+      value.innerHTML = companyString
+      //count--;
     } else if (styles.contains("increase")) {
-      count *= 2;
-      // count = count * 2
+      const companyName = companies[Math.floor(Math.random() * companies.length)]
+
+      const firstLetter = companyName.charAt(0)
+      const companyString = "My company is a" + ((vowels.indexOf(firstLetter) > -1) ? "n " : " ") + companyName + " for " + animals[Math.floor(Math.random() * animals.length)] + "s!<br />"
+      
+      value.innerHTML = companyString
+      //count *= 2;
     } else {
       count = 1;
     }
@@ -32,6 +49,6 @@ btns.forEach(function (btn) {
     if (count === 0) {
       value.style.color = "#222";
     }
-    value.textContent = count;
+    //value.textContent = count;
   });
 });
